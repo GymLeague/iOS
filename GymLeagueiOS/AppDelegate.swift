b429d2c3cdbc59685751bc21d66d01574d6f54da
adityamaru27 <a2maru@uwaterloo.ca>
2016-11-12 11:37:11 -0500

    friends setup

--- GymLeagueiOS/AppDelegate.swift
+++ GymLeagueiOS/AppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 import FBSDKLoginKit
-
+import Alamofire
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -18,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        
         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
 
         }
--- GymLeagueiOS/ViewController.swift
+++ GymLeagueiOS/ViewController.swift
@@ -8,19 +8,21 @@
 
 import UIKit
 import FBSDKLoginKit
+import Alamofire
 
 class ViewController: UIViewController {
     var dict : [String : AnyObject]!
 
     @IBAction func loginButton(_ sender: Any) {
         let fbLoginManager : FBSDKLoginManager = FBSDKLoginManager()
-        fbLoginManager.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+        fbLoginManager.logIn(withReadPermissions: ["email", "user_friends"], from: self) { (result, error) in
             if (error == nil){
                 let fbloginresult : FBSDKLoginManagerLoginResult = result!
                 if fbloginresult.grantedPermissions != nil {
                     if(fbloginresult.grantedPermissions.contains("email"))
                     {
                         self.getFBUserData()
+                        self.getFBFirends()
                         fbLoginManager.logOut()
                     }
                 }
@@ -42,10 +44,38 @@ class ViewController: UIViewController {
             FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
                 if (error == nil){
                     self.dict = result as! [String : AnyObject]
-                    print(result!)
-                    print(self.dict)
+                    var url = "http://159.203.141.33:51617/api/v1/login"
+                    let headers = [
+                        "Content-Type" : "application/x-www-form-urlencoded"
+                    ]
+                    let parameter = ["first_name" : self.dict["name"], "last_name" : self.dict["last_name"], "id" : self.dict["id"], "image" : self.dict["picture"]]
+                    //
+                    Alamofire.request(url, method: .post, parameters: parameter, encoding: URLEncoding.default, headers: headers) .response { (response) in
+                        print(response)
+                        
+                    }
                 }
             })
         }
-    }}
+    }
+    
+    func getFBFirends()
+    {
+        let params = ["fields": "id, first_name, last_name, middle_name, name, email, picture"]
+        let FBRequest = FBSDKGraphRequest(graphPath: "me/friends", parameters: params)
+        FBRequest?.start(completionHandler: { (connection, result, error) in
+            if error != nil
+            {
+                print("There was an error")
+                
+            }
+            else
+            {
+                print(result!)
+            }
+        })
+    
+    }
+
+}
 
